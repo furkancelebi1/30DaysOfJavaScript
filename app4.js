239c68082b795c220c8a6145ac20f185149128e2
Furkan Çelebi <ffurkancelebi@gmail.com>
2025-03-01 23:37:44 +0300

    function

---
+++ app4.js
@@ -0,0 +1,26 @@
+//! Fonksiyonlar
+
+//? Paramtresiz Fonksiyon
+function topla() {
+    console.log(5+9);
+}
+topla();
+
+//? Parametreli Fonksiyon
+function topla2(a, b) {
+    console.log(a+b);
+}
+topla2(213, 9);
+
+//? Geriye Değer Döndüren Fonksiyon
+
+function topla3(a, b) {
+    return a+b;
+}
+console.log(topla3(5, 9));
+
+//? Return bir değeri fonksiyonun dışına çıkartır.
+//? Return'den sonra bir işlem yazılmaz.
+//? Void fonksiyonlar geriye değer döndürmez.
+
+ 
