5883af321a6a073c4702c2cc8547e2234439e212
Furkan Çelebi <ffurkancelebi@gmail.com>
2025-03-04 22:14:45 +0300

    Arrays

---
+++ app5.js
@@ -0,0 +1,34 @@
+//! Array
+
+//? const arr = [1, 2, 3, 4, 5];
+//? console.log(arr);
+
+//! Array Methods
+
+//? push() - Diğer elemanları kaydırmadan bir dizinin sonuna bir veya daha fazla eleman ekler ve dizinin yeni uzunluğunu döndürür.
+//? unshift() - Diğer elemanları kaydırmadan bir dizinin başına bir veya daha fazla eleman ekler ve dizinin yeni uzunluğunu döndürür.
+//? pop() - Bir dizinin son elemanını kaldırır ve bu elemanı döndürür.
+//? shift() - Bir dizinin ilk elemanını kaldırır ve bu elemanı döndürür.
+//? splice() - Bir dizinin içindeki bir veya daha fazla elemanı kaldırır ve/veya yeni elemanlar ekler.
+//? slice() - Bir dizinin bir kısmını yeni bir dizi olarak döndürür.
+//? toString() - Bir diziyi bir stringe dönüştürür ve döndürür.
+//? join() - Bir dizinin tüm elemanlarını birleştirir ve bir string olarak döndürür.
+//? concat() - Bir veya daha fazla diziyi birleştirir ve yeni bir dizi döndürür.
+//? reverse() - Bir dizinin elemanlarını ters çevirir.
+//? sort() - Bir dizinin elemanlarını sıralar.
+//? indexOf() - Bir dizide belirli bir elemanın ilk indeksini döndürür.
+//? lastIndexOf() - Bir dizide belirli bir elemanın son indeksini döndürür.
+//? includes() - Bir dizide belirli bir elemanın olup olmadığını belirler ve true veya false döndürür.
+//? find() - Bir dizide belirli bir elemanı bulur ve döndürür.
+//? findIndex() - Bir dizide belirli bir elemanın indeksini döndürür.
+//? filter() - Bir dizide belirli bir koşulu karşılayan elemanları döndürür.
+//? map() - Bir dizideki tüm elemanlar üzerinde bir işlem yapar ve yeni bir dizi döndürür.
+//? reduce() - Bir dizideki tüm elemanlar üzerinde bir işlem yapar ve bir sonuç döndürür.
+//? some() - Bir dizide belirli bir koşulu karşılayan en az bir eleman varsa true, yoksa false döndürür.
+//? every() - Bir dizideki tüm elemanlar belirli bir koşulu karşılıyorsa true, yoksa false döndürür.
+
+//! Foreach
+
+//? arr.forEach((item, index) => {
+//?     console.log(index, item);
+//? });
