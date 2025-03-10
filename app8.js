bf85868b62afbc6ea837b2d0890cc73170eaa437
Furkan Çelebi <ffurkancelebi@gmail.com>
2025-03-10 22:23:58 +0300

    Document Object Model (DOM)

---
+++ app8.js
@@ -0,0 +1,33 @@
+//! Document Object Model (DOM) Manipulation
+
+//? DOM, bir HTML belgesini ağaç yapısında temsil eden bir API'dir.
+//? Bu API, HTML belgesindeki her bir öğeyi bir nesne olarak temsil eder
+//? ve bu nesneleri manipüle etmek için bir dizi metod ve özellik sağlar.
+
+//? DOM, JavaScript ile birlikte kullanıldığında, web sayfalarını dinamik hale getirmek için kullanılır.
+//? JavaScript, DOM'u kullanarak HTML öğelerini değiştirebilir, ekleyebilir ve silebilir.
+
+//? Document  - Sayfanın içeriğini döndürür.
+//? Location  - Sayfanın URL'sini döndürür.
+//? History   - Tarayıcının geçmişini döndürür.
+//? Navigator - Tarayıcı hakkında bilgi döndürür.
+
+//! Selectors (Seçiciler) 
+
+//? getElementById() - Bir id'ye göre bir HTML öğesini seçer.
+//? getElementsByClassName() - Bir sınıfa göre HTML öğelerini seçer.
+//? getElementsByTagName() - Bir etikete göre HTML öğelerini seçer.
+//? innerHTML - Bir HTML öğesinin içeriğini değiştirir.
+//? querySelector() - Bir CSS seçiciye göre bir HTML öğesini seçer.
+//? querySelectorAll() - Bir CSS seçiciye göre tüm HTML öğelerini seçer.
+
+//! Style Manipulation (Stil Manipülasyonu)
+
+//? style.property - Bir HTML öğesinin stil özelliğini değiştirir.
+//? children - Bir HTML öğesinin çocuklarını döndürür.
+//? firstElementChild - Bir HTML öğesinin ilk çocuğunu döndürür.
+//? lastElementChild - Bir HTML öğesinin son çocuğunu döndürür.
+//? nextElementSibling - Bir HTML öğesinin bir sonraki kardeşini döndürür.
+//? previousElementSibling - Bir HTML öğesinin bir önceki kardeşini döndürür.
+
+//! 
